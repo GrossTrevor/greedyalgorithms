29abfc8f69cea49504164af4f8acc178830e9108
GrossTrevor <grosstrevor000@gmail.com>
2026-03-02 16:41:21 -0500

    feat: added running lru

--- src/main.py
+++ src/main.py
@@ -3,12 +3,12 @@ import fifo, lru, optff
 def main():
     k, m, r = parse('data/input.in')
     fifo_misses = fifo.fifo(k, r)
-    # lru_misses = lru.lru(k, r)
+    lru_misses = lru.LRU_Test(k, r)
     # optff_misses = optff.optff(k, r)
 
     with open('data/output.out', 'w') as file:
         file.write(f'FIFO  : {fifo_misses}\n')
-        # file.write(f'LRU  : {lru_misses}\n')
+        file.write(f'LRU  : {lru_misses}\n')
         # file.write(f'OPTFF  : {optff_misses}\n')
 
 """
